ccba862c43f2f0ddf268bbc910d44529ab357d32
Andres Malfavaun <2203024384@alumnos.cua.uam.mx>
2023-10-05 22:49:07 -0600

    form

--- formulario.php
+++ formulario.php
@@ -15,7 +15,7 @@ try {
     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@avivamientointernacional.website';                     //SMTP username
-    $mail->Password   = 'Avivamiento#2023';                               //SMTP password
+    $mail->Password   = 'Avivamiento#2023#';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
