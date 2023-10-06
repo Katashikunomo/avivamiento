80ae4e4122bad4fee6379d9dde3365f0405fb25e
Andres Malfavaun <2203024384@alumnos.cua.uam.mx>
2023-10-05 23:36:12 -0600

    mas refactoring

--- admin/model/registro_evento.php
+++ admin/model/registro_evento.php
@@ -1,9 +1,18 @@
 <?php
-    include("../../controller/conexion.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["selected_date"]) && !empty($_POST["selected_date"])) {
         $selectedDate = $_POST["selected_date"];
-     
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "avivamiento";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Error de conexión: " . $conn->connect_error);
+        }
 
         // $sql = "INSERT INTO tb_fechas (fecha) VALUES ('$selectedDate')";
         // $sql = "SELECT * FROM tb_fechas";
