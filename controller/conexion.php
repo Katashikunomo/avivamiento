b12681258bd8f9d454fdfa4cc1c373b0e00b6a8d
Andres Malfavaun <2203024384@alumnos.cua.uam.mx>
2023-10-05 22:42:31 -0600

    con

--- controller/conexion.php
+++ controller/conexion.php
@@ -1,6 +1,11 @@
 <?php
 	//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	$mysqli = new mysqli("localhost","root","","avivamiento"); 
+	$servername = "localhost";
+	$dbname = "u247480952_aviva";
+	$username = "u247480952_avivamiento";
+	$password = "naycq=Psi9L@";  
+	$mysqli = new mysqli($servername, $username, $password, $dbname); 
+// 	$mysqli = new mysqli("localhost","u247480952_avivamiento","[A6pA&9]*U","u247480952_aviva"); 
 	
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
